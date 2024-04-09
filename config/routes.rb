32c29d45002c4c758867efba3627de6b60c1b0b0
jusn4 <164776044+jusn4@users.noreply.github.com>
2024-04-09 02:35:34 +0000

    [Finish]3-7

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,9 @@ Rails.application.routes.draw do
   post 'lists' => 'lists#create'
 
 Rails.application.routes.draw do
-  get 'lists/new'
+
   get 'top' => 'homes#top'
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
-  patch 'lists/:id' => 'lists#update', as: 'update_list'
+  resources :lists
 end
 
 Rails.application.routes.draw do
